c8e4680e45f22a4decefab055483cf8a0cfd42dc
Upol <Upol@DESKTOP-U0NT4HT>
2022-05-30 22:02:38 +0600

    Add project files.

---
+++ lab2/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using lab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Person pr = new Person();
+            pr.Id = 1;
+            pr.Name = "Tanvir";
+            pr.Dob = "19-39570-1";
+            return View(pr);
+        }
+    }
+}
